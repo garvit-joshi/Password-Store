598db10e5f1461d64420e01898983fe4393ca69a
Garvit Joshi <garvitjoshi9@gmail.com>
2020-05-20 16:01:07 +0530

    Added Functions for New User

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,84 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include <cstdlib>
 using namespace std;
-/*class Password{
-};*/
+class Data{
+	string name;
+	long long int Password;
+	int key,n;
+	public:
+	void setvalues(string nameuser,long long int p,int no){
+		Password=p;              //master password
+		key=rand()%10;               //key
+		name=nameuser;            
+		n=no;           //No. oF passwords
+	}
+	void addno(int no){
+		
+	}
+	void decreseno(int no){
+	
+	}
+	
+};
+class Credentials{
+	string company,username,password;
+	public:
+	void setData(){
+		cout<<"Enter The Companies Name:";
+		getline(cin,company);
+		cout<<"Enter Your Username/Email:";
+		getline(cin,username);
+		cout<<"Enter Your Password:";
+		getline(cin,password);
+	}
+	void printData(){
+		cout<<"The Companies Name:"<<company;
+		cout<<"\nYour Username/Email:"<<username;
+		cout<<"Your Password:"<<password;
+	}
+};
 int main()
 {
-	long long int pass;
+	int choice,n;
+	string Name;
+	long long int Password;
 	cout<<"\t\tWelcome To Password Store\n";
-	cout<<"\t\tPlease Enter Your Password:";
-	cin>>pass;
+	cout<<"Enter Your Choice:\n";
+	cout<<"1.New User\n2.Retrive Your Passwords\n3.Exit\n";
+	cin>>choice;
+	if(choice==1){
+		Data D;
+		cin.ignore();
+		cout<<"Please Enter Your Name:";
+		getline(cin,Name);
+		cout<<"Hello!! "<<Name<<" I will asist you in the process\n";
+		cout<<"How Many Passwords Do you want to save:";
+		cin>>n;
+		cout<<"Please Enter A master password(Integers Only):";
+		cin>>Password;
+		D.setvalues(Name,Password,n);
+		ofstream ofile("Data.txt",ios::out|ios::binary);
+		ofile.write((char*)&D,sizeof(D));
+		ofile.close();
+		Credentials a;
+		ofstream oofile("Credentials.txt",ios::out|ios::binary);
+		cin.ignore();
+		for(int i=0;i<n;i++){
+			a.setData();
+			oofile.write((char*)&a,sizeof(a));
+		}
+		oofile.close();
+	}
+	else if(choice==2){
+		vector <Credentials> User;
+		ifstream ifile("Credentials.txt",ios::in|ios::binary);
+		ifile.seekg(0);
+		ifile.read((char*)&User,sizeof(User));
+		for(int i=0;i<User.size();i++){
+			User[i].printData();
+		}
+	}
 	return 0;
 }
