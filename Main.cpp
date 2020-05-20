ec62d2f2bd043553edd1e7de9e89c5af4c4ac212
Garvit Joshi <garvitjoshi9@gmail.com>
2020-05-20 11:21:15 +0530

    First Commit

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+/*class Password{
+};*/
+int main()
+{
+	long long int pass;
+	cout<<"\t\tWelcome To Password Store\n";
+	cout<<"\t\tPlease Enter Your Password:";
+	cin>>pass;
+	return 0;
+}
