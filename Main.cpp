38c9ed6536d4ab2126960c692540a6d8fae2a729
Garvit Joshi <garvitjoshi9@gmail.com>
2020-06-21 21:58:21 +0530

    Made code more redable

--- Main.cpp
+++ Main.cpp
@@ -98,14 +98,14 @@ void outputCredentialData(int n)
 	Credentials a;
 	ifstream ifile("Credentials.txt", ios::in | ios::binary);
 	ifile.seekg(0);
-	cout<<"\n\n======================================================================\n";
+	cout << "\n\n======================================================================\n";
 	while (ifile.read((char*)&a, sizeof(a))) {
 		cout << "Entry " << EntryNumber << " of " << n;
 		a.printData();
-		cout<<"\n==================================================================\n\n";
+		cout << "\n==================================================================\n\n";
 		EntryNumber++;
 	}
-	cout<<"======================================================================\n\n";
+	cout << "======================================================================\n\n";
 	ifile.close();
 }
 
@@ -121,7 +121,7 @@ void inputCredentialData(int n, bool type = false)
 		***************************************************************/
 		ofstream ofile("Credentials.txt", ios::out | ios::binary | ios::trunc);
 		for (int i = 0; i < n; i++) {
-			cout << "\nEntry "<<i+1 << " of "<<n <<" \n";
+			cout << "\nEntry " << i + 1 << " of " << n << " \n";
 			a.setData();
 			ofile.write((char*)&a, sizeof(a));
 		}
@@ -131,7 +131,7 @@ void inputCredentialData(int n, bool type = false)
 	{
 		ofstream ofile("Credentials.txt", ios::out | ios::binary | ios::app);
 		for (int i = 0; i < n; i++) {
-			cout << "\nEntry "<<i+1 << " of "<<n <<" \n";
+			cout << "\nEntry " << i + 1 << " of " << n << " \n";
 			a.setData();
 			ofile.write((char*)&a, sizeof(a));
 		}
@@ -173,7 +173,7 @@ int deleteCredential(int an)
 {
 	/*
 	Will Delete A credential from the decrypted file.
-	The (an) argument in the function tells the number 
+	The (an) argument in the function tells the number
 	of the credential which has to be deleted.
 	*/
 	int count = 1, flag = 0;
@@ -196,8 +196,8 @@ int deleteCredential(int an)
 	}
 	tempfile.close();
 	Credentialsfile.close();
-	/* 
-	In the above code all the data except the record that has to be deleted 
+	/*
+	In the above code all the data except the record that has to be deleted
 	is written into Temporary file
 	*/
 	if (flag == 1)
@@ -285,23 +285,23 @@ int editCredential(int an)
 
 void About() {
 	system("CLS");
-	cout<<"\n\n\n";
-    cout<<"\t ||                                                                                         ||\n";
-    cout<<"\t=================================================================================================\n";
-    cout<<"\t ||                                  Password-Store                                         ||\n";
-    cout<<"\t ||                                               --Garvit Joshi                            ||\n";
-	cout<<"\t ||Dear Users,                                                                              ||\n";
-    cout<<"\t ||          Password-store is a Program that will store your password in files that will   ||\n";
-    cout<<"\t ||be protected in a file with AES-256 bit Encryption. Feel free to contact me on any       ||\n";
-	cout<<"\t ||reported bug. All the files are compiled and kept ready to use for the sake of your      ||\n";
-	cout<<"\t ||convenience.                                                                             ||\n";
-	cout<<"\t ||                                                                                         ||\n";
-    cout<<"\t=================================================================================================\n";
-    cout<<"\t ||  Git-Hub: https://github.com/garvit-joshi/Password-Store                                ||\n";
-	cout<<"\t ||  E-mail:  garvitjoshi9@gmail.com, garvitjoshi9@studentpartner.com                       ||\n";
-	cout<<"\t ||                                                                                         ||\n";
-    cout<<"\t=================================================================================================\n";
-    cout<<"\t ||                                                                                         ||\n";
+	cout << "\n\n\n";
+	cout << "\t ||                                                                                         ||\n";
+	cout << "\t=================================================================================================\n";
+	cout << "\t ||                                  Password-Store                                         ||\n";
+	cout << "\t ||                                               --Garvit Joshi                            ||\n";
+	cout << "\t ||Dear Users,                                                                              ||\n";
+	cout << "\t ||          Password-store is a Program that will store your password in files that will   ||\n";
+	cout << "\t ||be protected in a file with AES-256 bit Encryption. Feel free to contact me on any       ||\n";
+	cout << "\t ||reported bug. All the files are compiled and kept ready to use for the sake of your      ||\n";
+	cout << "\t ||convenience.                                                                             ||\n";
+	cout << "\t ||                                                                                         ||\n";
+	cout << "\t=================================================================================================\n";
+	cout << "\t ||  Git-Hub: https://github.com/garvit-joshi/Password-Store                                ||\n";
+	cout << "\t ||  E-mail:  garvitjoshi9@gmail.com, garvitjoshi9@studentpartner.com                       ||\n";
+	cout << "\t ||                                                                                         ||\n";
+	cout << "\t=================================================================================================\n";
+	cout << "\t ||                                                                                         ||\n";
 }
 
 
@@ -312,31 +312,31 @@ void systemHelp() {
 	************************************/
 	system("cls");
 	system("pip install pyAesCrypt");
-	cout<<"\n\n\n";
-	cout<<"\t ||                                                                                            ||\n";
-	cout<<"\t===================================================================================================\n";
-    cout<<"\t ||                                  Password-Store                                            ||\n";
-    cout<<"\t ||                                                                                            ||\n";
-	cout<<"\t ||Note: Some Supported Modules Need to be installed when using this application.              ||\n";
-	cout<<"\t ||                                                                                            ||\n";
-	cout<<"\t ||                                                                                            ||\n";
-	cout<<"\t ||Dear Users,                                                                                 ||\n";
-    cout<<"\t ||  If an error occurs saying \"'pip' is not recognized as an internal or external command,\"   ||\n";
-    cout<<"\t ||  please download and install python from python.org and add it to Environment variable.    ||\n";
-	cout<<"\t ||                                                                                            ||\n";
-    cout<<"\t====================================================================================================\n";
-	cout<<"\t ||  Git-Hub: https://github.com/garvit-joshi/Password-Store                                   ||\n";
-	cout<<"\t ||  E-mail:  garvitjoshi9@gmail.com, garvitjoshi9@studentpartner.com                          ||\n";
-	cout<<"\t ||                                                                                            ||\n";
-    cout<<"\t=================================================================================================\n";
-    cout<<"\t ||                                                                                            ||\n";
+	cout << "\n\n\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t===================================================================================================\n";
+	cout << "\t ||                                  Password-Store                                            ||\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t ||Note: Some Supported Modules Need to be installed when using this application.              ||\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t ||Dear Users,                                                                                 ||\n";
+	cout << "\t ||  If an error occurs saying \"'pip' is not recognized as an internal or external command,\"   ||\n";
+	cout << "\t ||  please download and install python from python.org and add it to Environment variable.    ||\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t====================================================================================================\n";
+	cout << "\t ||  Git-Hub: https://github.com/garvit-joshi/Password-Store                                   ||\n";
+	cout << "\t ||  E-mail:  garvitjoshi9@gmail.com, garvitjoshi9@studentpartner.com                          ||\n";
+	cout << "\t ||                                                                                            ||\n";
+	cout << "\t=================================================================================================\n";
+	cout << "\t ||                                                                                            ||\n";
 }
 
 
 
 int main()
 {
-	char ch = 'Y',ch1='Y';
+	char ch = 'Y', ch1 = 'Y';
 	/******************************
 	 *  This is menu driven program
 	*******************************/
@@ -344,7 +344,7 @@ int main()
 	cout << "\t\t\t\tWelcome To Password-Store \n\n\n";
 	while (ch == 'Y' || ch == 'y')
 	{
-		cout<<"\t\t\t\t\tMain Menu\n";
+		cout << "\t\t\t\t\tMain Menu\n";
 		cout << "1.New User\n2.Retrive Your Passwords\n3.About\n4.For installing supported modules(may require internet connection)\n";
 		cout << "Enter Your Choice:";
 		cin >> choice1;
@@ -357,7 +357,7 @@ int main()
 		else if (choice1 == 2) {
 			ifstream check1, check2;
 			system("Decryption.pyc");
-			while(ch1== 'Y' || ch1== 'y'){
+			while (ch1 == 'Y' || ch1 == 'y') {
 				check1.open("Data.txt");
 				if (!check1) {
 					cout << "Error 404: Data.txt Does Not Exist.\n\n";
@@ -375,7 +375,7 @@ int main()
 				Data D;
 				ifstream ifile("Data.txt", ios::in | ios::binary);
 				ifile.read((char*)&D, sizeof(D));
-				n=D.getValues();
+				n = D.getValues();
 				ifile.close();
 				outputCredentialData(n);
 				cout << "You can Perform These Functions:\n";
@@ -424,7 +424,7 @@ int main()
 		else if (choice1 == 3) {
 			About();
 		}
-		else if(choice1 == 4) {
+		else if (choice1 == 4) {
 			systemHelp();
 		}
 		cout << "Menu(Y/N):";
