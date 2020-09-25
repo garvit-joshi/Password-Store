fbb0ecf1b5f8a5f957dbc877030e314ddbe68bbc
garvit Joshi <garvitjoshi9@gmail.com>
2020-09-25 22:31:54 +0530

    Few Minor Changes in Python Code with PEP-8 and Workflow Update

--- src/Decryption.py
+++ src/Decryption.py
@@ -17,36 +17,35 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-import pyAesCrypt
 import os
-#for installing pyAesCrypt please run this command:pip install pyAesCrypt
-#or click "4" In Menu for Support.
+import pyAesCrypt
+#for installing pyAesCrypt please run this command:pip install pyAesCrypt.
 password=input("Please Enter Your Master Password:")
-flag=0
-bufferSize = 64 * 1024
+FLAG=0
+BUFFERSIZE = 64 * 1024
 try:
     encFileSize1 = os.stat("Data.txt.aes").st_size
     encFileSize2 = os.stat("Credentials.txt.aes").st_size
 except:
     print("Error 404: Encrypted Files Not Found")
-    flag=1
-if(flag==0):
+    FLAG=1
+if FLAG==0:
     with open("Data.txt.aes", "rb") as fIn1:
         try:
             with open("Data.txt", "wb") as fOut1:
-                pyAesCrypt.decryptStream(fIn1, fOut1, password, bufferSize, encFileSize1)
+                pyAesCrypt.decryptStream(fIn1, fOut1, password, BUFFERSIZE, encFileSize1)
             fOut1.close()
             fIn1.close()
         except ValueError:
             os.remove("Data.txt")
             print("An Error Is Encountered\n")
             print("\nError 401(@1): Password May be wrong")
-            flag=1
-if(flag==0):
+            FLAG=1
+if FLAG==0:
     with open("Credentials.txt.aes", "rb") as fIn2:
         try:
             with open("Credentials.txt", "wb") as fOut2:
-                pyAesCrypt.decryptStream(fIn2, fOut2, password, bufferSize, encFileSize2)
+                pyAesCrypt.decryptStream(fIn2, fOut2, password, BUFFERSIZE, encFileSize2)
             print("All Data has been decrypted successfully")
             fIn2.close()
             fOut2.close()
--- src/Encryption.py
+++ src/Encryption.py
@@ -17,38 +17,38 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-import pyAesCrypt
 import os
+import pyAesCrypt
 #for installing pyAesCrypt please run this command(cmd):pip install pyAesCrypt
-#or click "4" In Menu for Support.
 password=input("Please Enter A master password:")
 print("\nFor Your Ease, the program can store your password in a file(Password.txt)")
-choice=input("\n Do you want to save the file in Password.txt(Y/N):")
-if(choice=="Y" or choice == "y"):
+choice=input("\nDo you want to save the file in Password.txt(Y/N):")
+if choice in ('Y', 'y'):
     ofile=open("Password.txt","w")
     ofile.write("Password:"+password)
     print("\nPassword has been stored in Password.txt")
     ofile.write("\nPlease copy this password in a diary and delete this file Permanently.\n")
     ofile.close()
 else:
-    print("\nAs per your request, Password.txt will not be created. Please Memorize the password.\n")
-bufferSize = 64 * 1024
+    print("\nAs per your request, Password.txt will not be created.\n")
+    print("Please Memorize the password.\n")
+BUFFERSIZE = 64 * 1024
 try:
     with open("Data.txt", "rb") as fIn1:
         with open("Data.txt.aes", "wb") as fOut1:
-            pyAesCrypt.encryptStream(fIn1, fOut1, password, bufferSize)
+            pyAesCrypt.encryptStream(fIn1, fOut1, password, BUFFERSIZE)
     fIn1.close()
     fOut1.close()
     with open("Credentials.txt", "rb") as fIn2:
         with open("Credentials.txt.aes", "wb") as fOut2:
-            pyAesCrypt.encryptStream(fIn2, fOut2, password, bufferSize)
+            pyAesCrypt.encryptStream(fIn2, fOut2, password, BUFFERSIZE)
     print("\tFiles have been Encrypted")
     fIn2.close()
     fOut2.close()
 except:
     print("An Error Is Encountered")
     print("Error 404:May be Files are not present at the desired location")
-    if(choice == "Y" or choice == "y"):
+    if choice in ('Y', 'y'):
         os.remove("Password.txt")
         print("We Have Deleted Password.txt for your convenience as the File(s) could not be Encrypted.")
 input("\nPress Enter to continue...")
